ffdc1991718af03e4ce6df450fa564c430d9dbf3
gcaceres93 <gabbaa011@gmail.com>
2018-04-24 12:11:06 -0400

    Change of root directory

---
+++ account_analytic_operating_unit/models/purchase_order.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+from odoo.tools.float_utils import float_compare
+
+
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    operating_unit = fields.Many2one('operating.unit','Operating Unit')
+
+
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    def _prepare_invoice_line_from_po_line(self, line):
+        if line.product_id.purchase_method == 'purchase':
+            qty = line.product_qty - line.qty_invoiced
+        else:
+            qty = line.qty_received - line.qty_invoiced
+        if float_compare(qty, 0.0, precision_rounding=line.product_uom.rounding) <= 0:
+            qty = 0.0
+        taxes = line.taxes_id
+        invoice_line_tax_ids = line.order_id.fiscal_position_id.map_tax(taxes)
+        invoice_line = self.env['account.invoice.line']
+        data = {
+            'purchase_line_id': line.id,
+            'name': line.order_id.name + ': ' + line.name,
+            'origin': line.order_id.origin,
+            'uom_id': line.product_uom.id,
+            'product_id': line.product_id.id,
+            'account_id': invoice_line.with_context(
+                {'journal_id': self.journal_id.id, 'type': 'in_invoice'})._default_account(),
+            'price_unit': line.order_id.currency_id.with_context(date=self.date_invoice).compute(line.price_unit,
+                                                                                                 self.currency_id,
+                                                                                                 round=False),
+            'quantity': qty,
+            'discount': 0.0,
+            'account_analytic_id': line.account_analytic_id.id,
+            'analytic_tag_ids': line.analytic_tag_ids.ids,
+            'operating_unit': line.operating_unit,
+            'invoice_line_tax_ids': invoice_line_tax_ids.ids
+        }
+        account = invoice_line.get_invoice_line_account('in_invoice', line.product_id, line.order_id.fiscal_position_id,
+                                                        self.env.user.company_id)
+        if account:
+            data['account_id'] = account.id
+        return data
